ef41619e7b8641b2f61ea4b300cc036264524e3b
Parth santoki <143581307+Parthsantoki2003@users.noreply.github.com>
2025-08-09 10:43:00 +0530

    Create YoutubeVideoDownload.py

---
+++ YoutubeVideoDownload/YoutubeVideoDownload.py
@@ -0,0 +1,11 @@
+import yt_dlp
+
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+ydl_opts = {
+    'format': 'best',
+    'outtmpl': '%(title)s.%(ext)s'
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
